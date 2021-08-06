6a36699e7ac6f63554f19a2acaeeab9a6bf2a29e
dmaddu <durgasaratchandra.maddu@progress.com>
2021-08-06 14:25:17 +0530

    Adding error check

--- greetings.go
+++ greetings.go
@@ -2,7 +2,11 @@ package greetings
 
 import "fmt"
 
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+
+	if name == "" {
+		return "", fmt.Errorf("%v", "Invalid name, name should not be empty")
+	}
 	message := fmt.Sprintf("Hello %s", name)
-	return message
+	return message, nil
 }
